db843cc3f35aa4c9f3a21dc8d17e839b92d59ef6
Stama Eleazar <eleoatam10@gmail.com>
2020-03-22 12:07:13 -0500

    remove dice on roll on one

--- app.js
+++ app.js
@@ -42,12 +42,15 @@ function rollDiceFunction() {
     document.querySelector('#score-' + activePlayer).innerHTML = (globalScore)
 
 
-    document.querySelector('.dice').src = "dice-" + random + ".png"
+    
     if (random === 1) {
+        document.querySelector('.dice').style.display = "none"
         resetFunction()
         whichPlayer()
     } else {
         console.log('not 1')
+        document.querySelector('.dice').style.display = "block"
+        document.querySelector('.dice').src = "dice-" + random + ".png"
     }
 
 
@@ -120,16 +123,16 @@ function whichPlayer() {
         activePlayer = activePlayer + 1
         console.log('player 2 is next')
 
-        document.querySelector('.player-0-panel').classList.remove("active")
-        document.querySelector('.player-1-panel').classList.add("active")
+        document.querySelector('.player-0-panel').classList.toggle("active")
+        document.querySelector('.player-1-panel').classList.toggle("active")
 
 
 
     } else {
         activePlayer = 0
         console.log('player 1 is next')
-        document.querySelector('.player-1-panel').classList.remove("active")
-        document.querySelector('.player-0-panel').classList.add("active")
+        document.querySelector('.player-1-panel').classList.toggle("active")
+        document.querySelector('.player-0-panel').classList.toggle("active")
 
 
 
